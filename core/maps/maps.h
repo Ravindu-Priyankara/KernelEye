911569d49c47a027a8023a87fc128a72560a0040
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-02-24 12:21:46 +0530

    feat: Add hash map

---
+++ core/maps/maps.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// maps sizes
+#define RINGBUF_SIZE 1024
+#define HASHMAP_SIZE 4096
+
+/* Used hashmap for:
+*   1. track events
+*   2. stop streaming every events to userland
+*/
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);    // map type
+    __uint(max_entries, HASHMAP_SIZE);  // hashmap maximum entries
+    __type(key, __u32); // key = pid
+    __type(value, struct proc_state);   // events data holding struct
+}process_map SEC(".maps");  // hashmap name
+
