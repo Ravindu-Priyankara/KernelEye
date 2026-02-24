05f34e881631cc0fee14280a41dd4bafe45ac1e8
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-02-24 15:01:51 +0530

    feat: Add common structs: net event

--- core/common/common_structs.h
+++ core/common/common_structs.h
@@ -1,6 +1,11 @@
 #pragma once
 
-// This struct use for tracking events with hashmap
-struct proc_state{
-
+// This struct use for tracking connect events with hashmap
+struct net_event{
+    __u32 ppid; // parent process id {4 bytes}
+    __u32 ip;   // connect ip {4 bytes}
+    __u16 port; // connect port {2 byte}
+    __u64 ts;   // timestamp {8 byte}
+    // total = 18 bytes, need 6 bytes for alignment
+    __u8 padding[6]; // total 24 bytes
 };
