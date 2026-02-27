c708b3dd6af3306d38062f064ec533c22920ff55
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-02-27 16:30:09 +0530

    Feat: try to remove vmlinux.h

--- core/common/common_headers.h
+++ core/common/common_headers.h
@@ -1,10 +1,11 @@
 #pragma once
 
-// Core kernel structs + CO-RE (Compile Once – Run Everywhere)
-#include "vmlinux.h"
-
 // eBPF helpers & macros
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_endian.h> // for bpf_ntohs
+
+#include <linux/types.h>   // __u32, __u16, __u8, __u64
+#include <linux/in.h>      // sockaddr_in, sockaddr_in6
+#include <linux/socket.h>  // AF_INET, AF_INET6
