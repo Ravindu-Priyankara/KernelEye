3a9d19cbd89e223d2127249839fccc40c40b446a
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-02-27 19:09:42 +0530

    fix: try to solve clang and vmlinux typedef conflict

--- core/common/common_headers.h
+++ core/common/common_headers.h
@@ -1,12 +1,10 @@
 #pragma once
 
-#include <linux/types.h>   // __u32, __u16, __u8, __u64
+#include "vmlinux.h"
 
 // eBPF helpers & macros
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_endian.h> // for bpf_ntohs
-
-#include <linux/in.h>      // sockaddr_in, sockaddr_in6
-#include <linux/socket.h>  // AF_INET, AF_INET6
+#include "vmlinux_wrapper.h"
---
+++ core/common/vmlinux_wrapper.h
@@ -0,0 +1,3 @@
+#pragma once
+#define __WCHAR_TYPE_DEFINED 1
+#include "vmlinux.h"
