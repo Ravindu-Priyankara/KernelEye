8583460cba69d99078500fa51d683335eaa17bc8
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-02-27 15:55:03 +0530

    Fix: missing header file for bpf_ntohs

--- core/common/common_headers.h
+++ core/common/common_headers.h
@@ -7,6 +7,7 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
+#include <bpf/bpf_endian.h> // for bpf_ntohs
 
 // for socket based things
 #include <linux/socket.h>
