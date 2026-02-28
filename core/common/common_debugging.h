7101946a606804f530cadfc54c0e9dc43894645f
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-02-28 11:40:34 +0530

    BugFix: Fix debug map define issue

--- core/common/common_debugging.h
+++ core/common/common_debugging.h
@@ -2,15 +2,14 @@
 
 #ifdef DEBUG_MODE
 
-// Single-slot debug map
-struct bpf_map_def SEC("maps") debug_map = {
-    .type        = BPF_MAP_TYPE_ARRAY,
-    .key_size    = sizeof(__u32),
-    .value_size  = sizeof(__u64),
-    .max_entries = 1,
-};
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, __u32);
+    __type(value, __u64);
+} debug_map SEC(".maps");
 
-// Helper to increment counter or set a value
+// Increment counter helper
 static __always_inline void debug_counter(__u64 val) {
     __u32 key = 0;
     __u64 *curr = bpf_map_lookup_elem(&debug_map, &key);
@@ -21,6 +20,5 @@ static __always_inline void debug_counter(__u64 val) {
 }
 
 #else
-// If DEBUG_MODE is not set, empty inline (optimized out)
 static __always_inline void debug_counter(__u64 val) { }
 #endif
