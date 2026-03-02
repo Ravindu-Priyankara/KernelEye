4db478b1d6e786063587cc05abe234c22a4c8f64
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-03-02 10:56:51 +0530

    BugFix: Fixed forward declaration put wrong block

--- core/helpers/connect_helpers.h
+++ core/helpers/connect_helpers.h
@@ -10,6 +10,8 @@
 // Forward declarations
 static int __always_inline parse_ipv4(const void *usr_ptr, struct connect_event *event);
 static int __always_inline parse_ipv6(const void *usr_ptr, struct connect_event *event);
+static __always_inline void *check_hash_map_data_availability(void *map, const void *key);
+static int __always_inline update_hash_map_element(void *map, const void *key, const void *value, __u64 flags);
 
 /*
 *   This helper's main task is identify the socket family type and return it to the requester.
@@ -127,10 +129,7 @@ static int __always_inline parse_socket_address(const int family, const void *us
 *   Return 0 / -1 {0 = success, -1 = failure}
 */
 static __always_inline int copy_the_connect_event_data(__u32 pid){
-    //forward declarations
-    static __always_inline void *check_hash_map_data_availability(void *map, const void *key);
-    static int __always_inline update_hash_map_element(void *map, const void *key, const void *value, __u64 flags);
-    
+
     // event struct for copy 
     struct connect_event *event;
     // for hold return value
