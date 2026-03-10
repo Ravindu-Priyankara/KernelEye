0cf34c69503519ee7d70f9ce6138367c20d79dab
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-03-10 15:53:45 +0530

    feat: Add struct for hold detection functions

---
+++ core/detections/detector.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../common/common_structs.h"
+#include "detection_results.h"
+
+// This struct is used for store detection functions
+typedef struct ke_detector{
+    // for detector name
+    const char *name;
+
+    // function pointer for hold funtion name
+    int (*detect)(
+        // arg1 = event header 
+        // arg2 = detection result
+        struct ke_event_header *event,
+        struct detection_results * result
+    );
+} ke_detector;
