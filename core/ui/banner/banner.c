0691e2a9d418544890363a10d33e232cc93ce266
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-03-14 15:56:29 +0530

    bugfix: remove multiple times same function declaration

--- core/ui/banner/banner.c
+++ core/ui/banner/banner.c
@@ -25,25 +25,3 @@ void ke_description(void){
     printf("Author : Ravindu Priyankara\n");
     printf("Research Project\n\n");
 }
-
-
-void ke_display_init(void)
-{
-    printf("\033[2J"); // clear screen
-    printf("\033[H");  // move cursor to top
-
-    printf("=====================================================================\n");
-    printf(" KernelEye Live Threat Monitor\n");
-    printf("=====================================================================\n");
-
-    printf("%-8s %-8s %-10s %-20s %-10s\n",
-           "PID",
-           "TYPE",
-           "SEVERITY",
-           "DETECTION",
-           "SCORE");
-
-    printf("---------------------------------------------------------------------\n");
-
-    fflush(stdout);
-}
