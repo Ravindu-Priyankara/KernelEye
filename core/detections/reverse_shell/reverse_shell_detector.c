2c4103cfc655a6bfa4a5aaaa0da16b0856972cbf
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-03-17 11:59:07 +0530

    refact: Change detection logic

--- core/detections/reverse_shell/reverse_shell_detector.c
+++ core/detections/reverse_shell/reverse_shell_detector.c
@@ -7,29 +7,34 @@
 
 
 /*
-*   This function is used to check `connect` + `execve` trigger duration is under 60 seconds or not. And it helps a lot to prevent false positives.
+*   This function is used to check `connect` + `execve` trigger duration is under 5 seconds or not. 
 *   Arguments:
 *       1. execve syscall trigger time
 *       2. connect syscall trigger time
 *   Return:
 *       0 = success 
 *       1 = failure
+*   Developer note:
+*       - Attacker can keep delay. and it helps to lost 20 score.
+*       - dup3, fork() -> should monitor parent also.
 */
 int reverse_shell_time_correlation(
     __u64 execve_ts,
     __u64 net_ts,
+    __u64 dup2_ts,
+    __u8 redirects,
     struct ke_detection_result *result
 )
 {
     // prevent missing timestamps & also return empty result
     if(execve_ts == 0 || net_ts == 0){
-        fprintf(stderr,"[ERROR]. Missing timestamp\n");
+        fprintf(stderr,"[ERROR]. Missing timestamp\n"); // should be change otherwise it will be break the UI.{fix: just convert to log rather than just print.}
         return 1;
     }
 
     // prevent pass null pointers
     if(!result) {
-        fprintf(stderr, "[ERROR]. Detection result is null pointer!\n");
+        fprintf(stderr, "[ERROR]. Detection result is null pointer!\n"); // should be change otherwise it will be break the UI.{fix: just convert to log rather than just print.}
         return 1;
     }
 
@@ -37,30 +42,37 @@ int reverse_shell_time_correlation(
     *   This function is called when the kernel sees
     *       1. connect syscall
     *       2. execve syscall
-    *   Therefore, we should add a 40 score for this.
+    *       3. dup2 syscall
+    *   Therefore, we should add a 30 score for this.
     *   severity:
     *       1. for syscall pattern trigger = info
     */
-    result->detection_id = KE_DET_CONNECT_WITH_EXECVE; // TODO: In the future, we should change the detection id overwrite design bug.
-    result->score += 40;
+    result->detection_id = KE_DET_CONNECT_WITH_EXECVE; // TODO: In the future, we should change the detection id overwrite design bug.{fix: use bit flags `|=` and also it will effect to policies and ui.}
+    result->score += 30;
     result->severity = KE_SEV_INFO;
     result->detected = true;
 
     /*
-    *   Assumptions
-    *       1. An attacker can reorder the syscall pattern
-    *       2. If the attacker reorder syscall pattern, the delta value take huge positive number because of the `__u64` data type.
+    *   Usually kernel streamed every events have redirects >= 2.
     */
-    __u64 delta = (execve_ts > net_ts)? execve_ts - net_ts : net_ts - execve_ts;
-
-    // check is it under 60 seconds
-    if(delta < 60ULL * 1000000000ULL){
-        // This helps to decide policies
-        // severity = if it trigger quick enough = warning
-        result->detection_id = KE_DET_REVERSE_SHELL; // TODO: In the future, we should change the detection id overwrite design bug.
-        result->score += 20;
-        result->severity = KE_SEV_WARNING;
+    if(redirects >= 2){
+        // common pattern {stdin/out/err}
+        result->detection_id = KE_DET_REVERSE_SHELL;
+        result->score += 40;
+        result->severity = KE_SEV_INFO;
         result->detected = true;
+
+        // strong signal -> upgrade severity
+        if (execve_ts > dup2_ts && (execve_ts - dup_ts < 5ULL * 1000000000ULL)) {
+            result->score += 20;
+            result->severity = KE_SEV_CRITICAL;
+            // others not overwrite
+        }
+
+        // connect -> dup2 -> execve
+        if (net_ts <= dup2_ts && dup2_ts <= execve_ts) {
+            result->score += 10; // ordering bonus
+        }
     }
 
     return 0;
@@ -139,12 +151,14 @@ int detect_reverse_shell(
     /*
     *   Reverse shell Detection 01 
     *   Assumptions:
-    *       1. Check is that duration between `connect` + `execve` under 60 seconds or not?
-    *       2. The attacker can set a delay between syscalls, which is why we set 60 seconds rather than just 5 seconds.
+    *       1. Check is that duration between `connect` + `execve` under 5 seconds or not?
+    *       2. The attacker can set a delay between syscalls, but without that timing gap we already proved it is reverse shell.
     */
     err = reverse_shell_time_correlation(
         event->data.execve_ts,
         event->data.net_ts,
+        event->data.last_dup2_ts,
+        event->data.stdio_redirects,
         result
     );
 
@@ -162,6 +176,10 @@ int detect_reverse_shell(
         event->data.filename, 
         result
     );
+
+    // 100 is the highest score. so if its exeed we should fix that.
+    if (result->score > 100)
+    result->score = 100;
 
     if(err) return 0;
 
