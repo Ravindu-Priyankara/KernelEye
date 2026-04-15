db546a3cdf5636fc46e3a3c3b24c58a7f23a2848
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-04-15 10:29:44 +0530

    bugfix: general bug fix on testing functions

--- core/helpers/testing_helpers.h
+++ core/helpers/testing_helpers.h
@@ -1,11 +1,11 @@
 // for testing purposes
 
 static __always_inline void print_score(__u32 score, __u64 cid){
-    bpf_printk("CID: %d, Score %d \n", cid, score);
+    bpf_printk("CID: %llu, Score %u \n", cid, score);
 }
 
 static __always_inline void print_flags_and_score(__u64 cid, __u32 score, __u32 flags){
-    bpf_printk("CID: %d, Score: %d, Flags: 0x%x \n");
+    bpf_printk("CID: %llu, Score: %u, Flags: 0x%x \n", cid, score, flags);
 }
 
 // will be display sys/kernel/debug/tracing/trace_pipe
