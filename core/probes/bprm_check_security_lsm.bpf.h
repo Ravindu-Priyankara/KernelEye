4dc8570e0a3dad8ed255c9c9e46b65b6791455f7
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-04-15 09:25:37 +0530

    feat: Add bprm check security lsm hook

---
+++ core/probes/bprm_check_security_lsm.bpf.h
@@ -0,0 +1,38 @@
+
+SEC("lsm/bprm_check_security")
+int BPF_PROG(trace_process_execute, struct linux_binprm *bprm){
+
+    __u64 cid;
+    __u64 execve_ts;
+    __u32 pid;
+
+    struct ke_ctx_state *ke_state;
+
+    pid = get_tgid();
+    execve_ts = get_trigger_time();
+    if(sanitize_the_pid(pid) != ERR_SUCCESS) return 0;
+
+    if(get_or_create_cid(pid, &cid) != ERR_SUCCESS) return 0;
+
+    // get the context state
+    ke_state = bpf_map_lookup_elem(&ctx_state_map, &cid);
+    if(!ke_state){
+        struct ke_ctx_state ke_new_state = {};
+
+        bpf_map_update_elem(&ctx_state_map, &cid, &ke_new_state, BPF_NOEXIST);
+        ke_state = bpf_map_lookup_elem(&ctx_state_map, &cid);
+        if(!ke_state) return 0;
+    }
+
+    ke_state->last_time = execve_ts;
+    if(!(ke_state->flags & EXECVE_SEEN)){
+        ke_state->flags |= EXECVE_SEEN;
+        ke_state->score += 5;
+    }
+
+    #ifdef DEBUG_MODE
+        debug_counter(1);
+    #endif
+
+    return 0;
+}
