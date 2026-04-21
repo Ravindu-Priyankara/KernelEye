8c78798bbff6519f309e5db0ce7866dd9da2b6fa
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-04-21 15:44:04 +0530

    feat: linke the event helper to bpf program

--- core/bpf/kerneleye.bpf.c
+++ core/bpf/kerneleye.bpf.c
@@ -5,6 +5,7 @@
 #include "../common/common_debugging.h"
 #include "../common/common_syscalls.h"
 
+#include "../helpers/event_helpers.h"
 #include "../helpers/common_helpers.h"
 #include "../helpers/connect_helpers.h"
 #include "../helpers/score_helpers.h"
