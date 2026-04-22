dabcafbabfd91b14ec01fb163b10d816501a8767
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-04-22 19:54:35 +0530

    refact: redesign process exit logic for fix detection blindspot

--- core/probes/sched_process_exit_tracker.bpf.h
+++ core/probes/sched_process_exit_tracker.bpf.h
@@ -9,16 +9,26 @@ int sched_process_exit_handler(struct trace_event_raw_sched_process_exit *ctx){
     __u64 *cid;
     __u64 pid;
 
-    pid = get_tgid();
+    struct ke_ctx_state *ke_state;
+
+    pid = ctx->pid; // stable rather than bpf helper
     if(sanitize_the_pid(pid) != ERR_SUCCESS) return 0;
     cid = bpf_map_lookup_elem(&ctx_map, &pid);
     if(!cid) return 0;
 
-    // for test
-    bpf_map_delete_elem(&ctx_state_map, cid);
-    bpf_map_delete_elem(&execve_hash_map, &cid);
-    bpf_map_delete_elem(&dup_map, &cid);
-    bpf_map_delete_elem(&connect_map, &cid);
+    // extract and clear context state map
+    ke_state = bpf_map_lookup_elem(&ctx_state_map, cid);
+    if(ke_state){
+        ke_state->last_time = 0;
+        ke_state->flags = 0;
+        ke_state->stage = STAGE_NORMAL;
+        ke_state->fd_mutation_count = 0;
+    }
+    
+    bpf_map_delete_elem(&execve_hash_map, cid);
+    bpf_map_delete_elem(&dup_map, cid);
+    bpf_map_delete_elem(&connect_map, cid);
+    bpf_map_delete_elem(&ctx_map, &pid); // otherwise innocent process will stay same cid
 
     return 0;
 }
